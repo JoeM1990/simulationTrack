2f447b7571bf5778405b3968a329a8ab8cb8e55e
Joe Monkila <jnkiwa25@gmail.com>
2024-09-11 09:34:23 +0100

    configure server and script

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket('ws://localhost:8080');
+const socket = new WebSocket('wss://simulationtrack.onrender.com/'); 
 
 document.addEventListener('DOMContentLoaded', () => {
     const toggleButtons = document.querySelectorAll('.toggle-btn');
