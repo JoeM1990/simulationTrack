3d36f34b7c0292c3ed9d96a49868471b3db09097
Joe Monkila <jnkiwa25@gmail.com>
2024-09-11 08:16:54 +0100

    script

--- scripts/script.js
+++ scripts/script.js
@@ -20,18 +20,5 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-  // Page d'état visuel des lampes
-  if (lampStatuses.length > 0) {
-    socket.onmessage = function(event) {
-      const data = JSON.parse(event.data);
-      const lampStatusElement = document.getElementById(`${data.lampId}-status`);
-      
-      if (lampStatusElement) {
-        lampStatusElement.classList.toggle('on', data.state);
-        lampStatusElement.classList.toggle('off', !data.state);
-        lampStatusElement.querySelector('.lamp-light').classList.toggle('on', data.state);
-        lampStatusElement.querySelector('.lamp-light').classList.toggle('off', !data.state);
-      }
-    };
-  }
+  
 });
